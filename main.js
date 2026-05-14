916799d2d6dad64188656bd6691761628d680261
luluhksjj <luiza.proenca.almeida@escola.pr.gov.br>
2026-05-14 15:28:39 +0000

    correção

---
+++ main.js
@@ -0,0 +1,9 @@
+const botoes = document.querySelectorAll(".botao");
+const abas = document.querySelectorAll(".aba-conteudo");
+
+for (let i = 0; i < botoes.length; i++) {
+  botoes[i].onclick = function () {
+    for (let j = 0; j < botoes.length; j++) {
+      botoes[j].classList.remove("ativo");
+      abas[j].classList.remove("ativo");
+    }''
